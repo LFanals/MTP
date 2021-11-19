6572488028fe8caf45fb799e859210351ad7c1dd
LFanals <lfanalsb@gmail.com>
2021-11-19 19:57:56 +0100

    Updated parent

--- integration/Switches/parent.py
+++ integration/Switches/parent.py
@@ -1,9 +1,9 @@
 # PCB map
 #                          SW1 - 1: ON, 0: OFF
 #                          SW2 - 1: TX, 0: RX
-#                          SW3 - may not be available to give space to the SD card
-#                          SW4 - 1: NM, 0: MRM|SR
-#                          SW5 - 1: MRM, 0: SR
+#                          SW3 - 1: NM, 0: MRM|SR
+#                          SW4 - 1: MRM, 0: SR
+#                          SW5 - X: to determine
 # LED1 LED2 LED3 LED4 LED5
 
 import RPi.GPIO as GPIO                           
@@ -27,19 +27,11 @@ SW3 = 11
 SW4 = 7
 SW5 = 5
 
-# Initialize states
-# iSWx and iiSWx act as two chained registers. rSWx changes state if iSWx==iiSWx
-rSW1, iSW1, iiSW1 = 0, 0, 0
-rSW2, iSW2, iiSW2 = 0, 0, 0
-rSW3, iSW3, iiSW3 = 0, 0, 0
-rSW4, iSW4, iiSW4 = 0, 0, 0
-rSW5, iSW5, iiSW5 = 0, 0, 0
-
 
 def config():
     # Config
-    GPIO.setmode(GPIO.BOARD)                              #BOARD selected so I can simply count the pins
-    GPIO.setwarnings(False)                               #Disable GPIO warnings
+    GPIO.setmode(GPIO.BOARD) # BOARD selected so I can simply count the pins
+    GPIO.setwarnings(False) # Disable GPIO warnings
     
     # Outputs
     GPIO.setup(LED1, GPIO.OUT)
@@ -54,64 +46,68 @@ def config():
     GPIO.output(LED4, GPIO.LOW)
     GPIO.output(LED5, GPIO.LOW)
     
-    # Inputs     
-    GPIO.setup(SW1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(SW2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(SW3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(SW4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(SW5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # Inputs. If SW is open there's a 1 at the input
+    GPIO.setup(SW1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(SW2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(SW3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(SW4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(SW5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
      
     print("Configured IO")    
 
 
 def read_switches():
+    # Reads inputs two times, with a 100ms delay between, to avoid metastability
+    # Returns boolean to indicate if all switches are stable and array with values
     iSW1 = GPIO.input(SW1)
     iSW2 = GPIO.input(SW2)
     iSW3 = GPIO.input(SW2)
     iSW4 = GPIO.input(SW4)
     iSW5 = GPIO.input(SW5)
 
-    iiSW1 = iSW1
-    iiSW2 = iSW2
-    iiSW3 = iSW3
-    iiSW4 = iSW4
-    iiSW5 = iSW5
+    time.sleep(0.1)
 
-    if (iSW1 == iiSW1): rSW1 = iSW1
-    if (iSW2 == iiSW2): rSW2 = iSW2
-    if (iSW3 == iiSW3): rSW3 = iSW3
-    if (iSW4 == iiSW4): rSW4 = iSW4
-    if (iSW5 == iiSW5): rSW5 = iSW5
- 
-    print("Read switches")
+    iiSW1 = GPIO.input(SW1)
+    iiSW2 = GPIO.input(SW2)
+    iiSW3 = GPIO.input(SW2)
+    iiSW4 = GPIO.input(SW4)
+    iiSW5 = GPIO.input(SW5)
+
+    if (iSW1 == iiSW1 and iSW2 == iiSW2 and iSW3 == iiSW3 and iSW4 == iiSW4 and iSW5 == iiSW5):
+        return True, [iSW1, iSW2, iSW3, iSW4, iSW5] 
+    else:
+        return False, [0, 0, 0, 0, 0]
 
 
-def read_master_sw():
-
-    read_switches()
-    print("Got master switch: ", str(rSW1))
-
-    if (rSW1 == 1): 
-        return True
-    elif (rSW1 == 0): 
+def is_master_on():
+    # Returns boolean to indicate SW1 state
+    state, SW = read_switches()
+    if not(state):
         return False
-
+    else:
+        print("Got master switch: ", str(SW[0]))
+        if (SW[0]) == 1):
+            return True
+        else:
+            return False
+            
 
 def control_led(led: int, state: bool):
+    # Control led number with a boolean: True = led on
     if (led == 1):
-        if (bool): GPIO.output(LED1, GPIO.HIGH)
+        if (state): GPIO.output(LED1, GPIO.HIGH)
         else: GPIO.output(LED1, GPIO.LOW)
     elif (led == 2):
-        if (bool): GPIO.output(LED2, GPIO.HIGH)
+        if (state): GPIO.output(LED2, GPIO.HIGH)
         else: GPIO.output(LED2, GPIO.LOW)
     elif (led == 3):
-        if (bool): GPIO.output(LED3, GPIO.HIGH)
+        if (state): GPIO.output(LED3, GPIO.HIGH)
         else: GPIO.output(LED3, GPIO.LOW)
     elif (led == 4):
-        if (bool): GPIO.output(LED4, GPIO.HIGH)
+        if (state): GPIO.output(LED4, GPIO.HIGH)
         else: GPIO.output(LED4, GPIO.LOW)
     elif (led == 5):
-        if (bool): GPIO.output(LED5, GPIO.HIGH)
+        if (state): GPIO.output(LED5, GPIO.HIGH)
         else: GPIO.output(LED1, GPIO.LOW)
     else:
         print("Bad led number")
@@ -120,11 +116,11 @@ def control_led(led: int, state: bool):
 
 
 def main():
-    config()
-    while (read_master_sw() == False):
+    config() # Configurate switches as inputs and leds as outputs
+    while (is_master_on() == False):
         sleep(0.1)  
 
-    read_switches() # get switches config, decide which son to run
+    state, SW = read_switches() # get switches config, decide which son to run, add logic below
     son.main()
 
 
--- integration/Switches/son.py
+++ integration/Switches/son.py
@@ -1,10 +1,11 @@
 # PCB map
 #                          SW1 - 1: ON, 0: OFF
 #                          SW2 - 1: TX, 0: RX
-#                          SW3 - may not be available to give space to the SD card
-#                          SW4 - 1: NM, 0: MRM|SR
-#                          SW5 - 1: MRM, 0: SR
+#                          SW3 - 1: NM, 0: MRM|SR
+#                          SW4 - 1: MRM, 0: SR
+#                          SW5 - X: to determine
 # LED1 LED2 LED3 LED4 LED5
+
 
 import RPi.GPIO as GPIO                           
 import time
@@ -18,7 +19,7 @@ def main():
         print("son loop") 
         parent.control_led(2, True)
 
-        if (read_master_sw() == False): # Master switch goes to 0
+        if (read_master_sw() == False): # Master switch goes to 0, the son programs have to stop
             print("Son execution finished")
 
         sleep(0.1)  
