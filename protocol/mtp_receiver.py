83f4449a60c5ab684154c268a899a22bd8412520
riscuus <riscoalbert@gmail.com>
2021-11-26 20:35:28 +0100

    Twerk

--- protocol/mtp_receiver.py
+++ protocol/mtp_receiver.py
@@ -231,9 +231,9 @@ def wait_chunk_is_good(nrf: NRF24, good: bool, id: int):
 
 def set_next_ack(nrf: NRF24, positive, type: int, id: int):
     if positive:
-        nrf.ack_payload(RF24_RX_ADDR.P1, [0, type, id])
+        nrf.ack_payload(RF24_RX_ADDR.P1, [(1).to_bytes(1, "big"), type.to_bytes(1, "big"), id.to_bytes(1, "big")])
     else:
-        nrf.ack_payload(RF24_RX_ADDR.P1, [0, type, id])
+        nrf.ack_payload(RF24_RX_ADDR.P1, [(0).to_bytes(1, "big"), type.to_bytes(1, "big"), id.to_bytes(1, "big")])
 
 def wait_data(nrf: NRF24):
     # print("Waiting for new data...")
