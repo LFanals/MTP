be9f913ceafbc6ce461d05a83c9cbe815e4dda61
adrifebrer <93275307+adrifebrer@users.noreply.github.com>
2021-11-29 16:59:55 +0100

    Update utils.py

--- f-protocol/utils.py
+++ f-protocol/utils.py
@@ -17,7 +17,8 @@ PA_LEVEL = RF24.RF24_PA_HIGH
 # DATA_RATE = RF24.RF24_1MBPS
 DATA_RATE = RF24.RF24_250KBPS
 # CHANNEL = 0x4c
-CHANNEL = 0x1b
+# CHANNEL = 0x1b
+CHANNEL = 0x3f
 RETRY_DELAY = 3 # between 0 and 15 (0 = 250us and 15 = 15 * 250us = 3.750ms)
 RETRY_COUNT = 5 # Between 0 and 15 (with 0 the retry functionality is disabled)
 
