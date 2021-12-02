c01d4f3886e6408cb8e2f9a81a7552777953e828
lfanals <lfanalsb@gmail.com>
2021-12-02 21:10:20 +0000

    sudo and rm media-usb

--- protocol/read_usb.sh
+++ protocol/read_usb.sh
@@ -9,6 +9,7 @@ device=$(sudo fdisk -l | grep -o '^/dev/sd[a-z][0-9]') # identify /dev/sda1 or s
 echo -e "Device: ${device} \n"
 
 echo -e "Mount USB device"
+sudo rm -r ${usb_dir}
 sudo mkdir ${usb_dir} # prepare mounting directory
 sudo chmod 777 ${usb_dir} # assign full permissions
 sudo mount ${device} ${usb_dir} # mount the device to the dedicated folder
@@ -17,9 +18,9 @@ echo -e "\n USB device files:"
 ls ${usb_dir} # check usb files
 
 echo -e "\n Copy files USB device -> working directory"
-rm -r ${working_dir}
-mkdir ${working_dir}
-cp ${usb_dir}/* ${working_dir}/ # copy present files 
+sudo rm -r ${working_dir}
+sudo mkdir ${working_dir}
+sudo cp ${usb_dir}/* ${working_dir}/ # copy present files 
 
 echo -e "\n Copied files USB device -> working directory:"
 ls ${working_dir} # check files
