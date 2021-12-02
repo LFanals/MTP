7be706cd514cb8d0baea2bb00bff66460c7a5fcc
riscuus <riscoalbert@gmail.com>
2021-12-02 23:58:50 +0100

    read doesn't remove media/usb

--- protocol/read_usb.sh
+++ protocol/read_usb.sh
@@ -9,7 +9,7 @@ device=$(sudo fdisk -l | grep -o '^/dev/sd[a-z][0-9]') # identify /dev/sda1 or s
 echo -e "Device: ${device} \n"
 
 echo -e "Mount USB device"
-sudo rm -r ${usb_dir}
+#sudo rm -r ${usb_dir}
 sudo mkdir ${usb_dir} # prepare mounting directory
 sudo chmod 777 ${usb_dir} # assign full permissions
 sudo mount ${device} ${usb_dir} # mount the device to the dedicated folder
