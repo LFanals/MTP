f6919ab225a18808577bfcd64b49c87e5f4d8b9d
Albert Ferraté Cuartero <albertferratecuartero@Alberts-MacBook-Pro.local>
2021-12-08 19:38:24 +0100

    WIP

--- protocol/main.py
+++ protocol/main.py
@@ -12,14 +12,21 @@ def main():
     ioparent.config()
 
     status = 1
+    led_active = True
     ioparent.reset_leds()
     ioparent.control_led(1, True)
     while status != 0:
 
+        while ioparent.is_master_on():
+            sleep(0.5)  
+            ioparent.control_led(3, led_active)
+            led_active = not led_active
+        ioparent.control_led(3, False)
+
         is_usb_read = False 
         while not ioparent.is_master_on():
-            if not is_usb_read and ioparent.is_usb_switch_on():
-                os.system("sudo bash " + utils.MTP_DIR + "read_usb.sh")
+            if not is_usb_read:
+                os.system("bash " + utils.MTP_DIR + "read_usb.sh")
                 if working_directory_contains_file():   
                     is_usb_read = True
                     ioparent.control_led(5, True)
@@ -43,7 +50,7 @@ def main():
 
             if is_TX:
                 while not working_directory_contains_file():
-                    os.system("sudo bash " + utils.MTP_DIR + "read_usb.sh")
+                    os.system("bash " + utils.MTP_DIR + "read_usb.sh")
         
                 print("SW[1] == 1 --> Starting communcation as: SENDER")
                 status = start_sender()
@@ -54,7 +61,7 @@ def main():
                 status = start_receiver()
                 ioparent.reset_leds()
                 ioparent.control_led(1, True)
-                os.system("sudo bash " + utils.MTP_DIR + "write_usb.sh")
+                os.system("bash " + utils.MTP_DIR + "write_usb.sh")
         
             if status: 
                 print("\nMANUAL INTERRUPT")
