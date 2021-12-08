8978b655c4a53885afdd10c5912fce976a8c7c10
lfanals <lfanalsb@gmail.com>
2021-12-08 14:22:34 +0000

    kill

--- protocol/kill.sh
+++ protocol/kill.sh
@@ -14,13 +14,22 @@ then
     sudo kill -9 ${pid}
 fi
 
-pid=$(ps ax | grep '.sh' | grep -v grep | awk '{print $1}')
+pid=$(ps ax | grep 'read_usb.sh' | grep -v grep | awk '{print $1}')
 
 if [ "$pid" != "" ];
 then
     echo "kill " ${pid}
     sudo kill -9 ${pid}
 fi
+
+pid=$(ps ax | grep 'write_usb.sh' | grep -v grep | awk '{print $1}')
+
+if [ "$pid" != "" ];
+then
+    echo "kill " ${pid}
+    sudo kill -9 ${pid}
+fi
+
 
 
 
